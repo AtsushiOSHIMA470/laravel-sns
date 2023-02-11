e8c2c3c0172fce05ca9bbb2ab1d513cb065c6b8c
Atsushi Oshima <atsushi.oshima.470@gmail.com>
2023-02-11 09:50:09 +0900

    Revert "fix"
    This reverts commit 48dd7cc4eed3ae520d4e2833fea387df7a3436a8.

--- app/Modules/ImageUpload/CloudinaryImageManager.php
+++ app/Modules/ImageUpload/CloudinaryImageManager.php
@@ -16,7 +16,7 @@ class CloudinaryImageManager implements ImageManagerInterface
      */
     public function save($file): string
     {
-        retrun cloudinary
+        retrun $this->cloudinary
         ->uploadApi()
         ->upload(is_string($file) ? $file : $file->getRealPath())['public_id'];
     }
@@ -26,6 +26,6 @@ class CloudinaryImageManager implements ImageManagerInterface
      */
     public function delete(string $name): void
     {
-        cloudinary->uploadApi()->destroy($name);
+        $this->cloudinary->uploadApi()->destroy($name);
     }
 }
